359207ccefb7eeea3d67a803e40d941e8ba2a6ec
shuai.yang <shuai.yang@corp.to8to.com>
2020-05-06 20:01:59 +0800

    初始化Spring框架

---
+++ src/main/java/com/demonstrate/java/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.demonstrate.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shuai.yang
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
